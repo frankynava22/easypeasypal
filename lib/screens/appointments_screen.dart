fc69231e9b63b022823f8075ec5f98263c35e900
Gregg Moscosa <gregitis@outlook.com>
2023-10-12 07:27:45 -0500

    Firestore DB set up COMPLETE

--- lib/screens/appointments_screen.dart
+++ lib/screens/appointments_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AppointmentScreen extends StatefulWidget { // Corrected class name
-  
+class AppointmentScreen extends StatefulWidget {
   AppointmentScreen();
+
   @override
   _AppointmentScreenState createState() => _AppointmentScreenState();
 }
 
 class _AppointmentScreenState extends State<AppointmentScreen> {
-   
+  final FirebaseFirestore firestore = FirebaseFirestore.instance; // Initialize Firestore
+
+  // Function to add a new appointment to the "Appointments" collection
+  Future<void> addAppointmentToFirestore(String date, String title) async {
+    await firestore.collection('Appointments').add({
+      'date': date,
+      'title': title,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +36,13 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            // Add your appointments-related content here
+            ElevatedButton(
+              onPressed: () {
+                // Call the function to add an appointment to Firestore
+                addAppointmentToFirestore('2023-10-15', 'Sample Appointment');
+              },
+              child: Text('Add Appointment to Firestore'),
+            ),
           ],
         ),
       ),
