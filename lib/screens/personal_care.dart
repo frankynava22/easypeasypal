c07ca30c002d0e37b219285298439ca9e90a1fbd
Daniel Garcia <daniel.garcia39@utrgv.edu>
2023-10-20 14:31:11 -0500

    Personal care rework

--- lib/screens/personal_care.dart
+++ lib/screens/personal_care.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class PersonalCareScreen extends StatefulWidget {
   @override
@@ -8,104 +6,6 @@ class PersonalCareScreen extends StatefulWidget {
 }
 
 class _PersonalCareScreenState extends State<PersonalCareScreen> {
-  final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
-
-  String? _weight;
-  String? _height;
-  String? _bloodPressure;
-  String? _bloodSugar;
-  String? _notes;
-
-  Stream<DocumentSnapshot> _metricsStream() {
-    final userId = _auth.currentUser!.uid;
-    final docReference = _firestore
-        .collection('users')
-        .doc(userId)
-        .collection('personal_care')
-        .doc('metrics');
-
-    docReference.get().then((doc) {
-      if (!doc.exists) {
-        docReference.set({
-          'weight': 'Enter weight',
-          'height': 'Enter height',
-          'bloodPressure': 'Enter blood pressure',
-          'bloodSugar': 'Enter blood sugar',
-          'notes': 'Enter notes',
-        });
-      }
-    });
-
-    return docReference.snapshots();
-  }
-
-  Future<void> _saveMetricToFirestore(String field, String value) async {
-    final userId = _auth.currentUser!.uid;
-    await _firestore
-        .collection('users')
-        .doc(userId)
-        .collection('personal_care')
-        .doc('metrics')
-        .update({field: value});
-  }
-
-  Future<void> _showMetricDialog({
-    required String title,
-    required String labelText,
-    required Function(String?) onValueChanged,
-    TextInputType inputType = TextInputType.text,
-    int? maxLines,
-  }) async {
-    final controller = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: TextField(
-          controller: controller,
-          keyboardType: inputType,
-          maxLines: maxLines,
-          decoration: InputDecoration(labelText: labelText),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text('Cancel'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          TextButton(
-            child: Text('Save'),
-            onPressed: () {
-              onValueChanged(controller.text.trim());
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMetricCard(String title, String? value, VoidCallback onTap) {
-    return Card(
-      elevation: 3,
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-        subtitle: Text(value ?? "No data", style: TextStyle(fontSize: 16)),
-        trailing: IconButton(
-          icon: Icon(Icons.edit, color: Colors.blueGrey),
-          onPressed: onTap,
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,113 +13,77 @@ class _PersonalCareScreenState extends State<PersonalCareScreen> {
         title: Text('Personal Care', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.teal,
       ),
-      backgroundColor: Colors.grey[100],
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _showMetricDialog(
-            title: "Enter weight (lb)",
-            labelText: 'Weight (lb)',
-            inputType:
-                TextInputType.numberWithOptions(signed: false, decimal: false),
-            onValueChanged: (value) {
-              if (value != null) {
-                _saveMetricToFirestore('weight', value);
-              }
-            },
-          );
-        },
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: _metricsStream(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          final docData = snapshot.data?.data() as Map<String, dynamic>?;
-
-          if (docData == null) {
-            return Center(child: Text('No data available.'));
-          }
-
-          return ListView(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            children: [
-              _buildMetricCard(
-                'Weight (lb)',
-                docData['weight'],
-                () => _showMetricDialog(
-                  title: "Enter weight (lb)",
-                  labelText: 'Weight (lb)',
-                  inputType: TextInputType.numberWithOptions(
-                      signed: false, decimal: false),
-                  onValueChanged: (value) {
-                    if (value != null) {
-                      _saveMetricToFirestore('weight', value);
-                    }
-                  },
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.teal[100]!, Colors.grey[300]!],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                  "Wellness Overview",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              _buildMetricCard(
-                'Height',
-                docData['height'],
-                () => _showMetricDialog(
-                  title: "Enter Height",
-                  labelText: "Height (e.g. 5'5)",
-                  onValueChanged: (value) {
-                    if (value != null) {
-                      _saveMetricToFirestore('height', value);
-                    }
-                  },
-                ),
-              ),
-              _buildMetricCard(
-                'Blood Pressure',
-                docData['bloodPressure'],
-                () => _showMetricDialog(
-                  title: "Enter Blood Pressure",
-                  labelText: 'e.g. 120/80',
-                  onValueChanged: (value) {
-                    if (value != null) {
-                      _saveMetricToFirestore('bloodPressure', value);
-                    }
-                  },
-                ),
-              ),
-              _buildMetricCard(
-                'Blood Sugar Level (mg/dL)',
-                docData['bloodSugar'],
-                () => _showMetricDialog(
-                  title: "Enter Blood Sugar Level",
-                  labelText: 'mg/dL',
-                  inputType: TextInputType.numberWithOptions(
-                      signed: false, decimal: false),
-                  onValueChanged: (value) {
-                    if (value != null) {
-                      _saveMetricToFirestore('bloodSugar', value);
-                    }
-                  },
-                ),
-              ),
-              _buildMetricCard(
-                'Notes',
-                docData['notes'],
-                () => _showMetricDialog(
-                  title: "Enter Notes",
-                  labelText: 'Notes',
-                  maxLines: 4,
-                  onValueChanged: (value) {
-                    if (value != null) {
-                      _saveMetricToFirestore('notes', value);
-                    }
-                  },
+              Expanded(
+                child: ListView(
+                  children: [
+                    _buildMetricCard(
+                        'Full Name', 'John Doe', Icons.account_circle),
+                    _buildMetricCard('Blood Type', 'O+', Icons.opacity),
+                    _buildMetricCard('Weight', '68kg', Icons.fitness_center),
+                    _buildMetricCard(
+                        'Blood Pressure', '120/80', Icons.favorite),
+                    _buildMetricCard(
+                        'Dietary Preference', 'Vegetarian', Icons.restaurant),
+                    _buildMetricCard(
+                        'Activity Level', 'Active', Icons.run_circle),
+                    _buildMetricCard(
+                        'Health Goals', 'Lose 5kg', Icons.track_changes),
+                    _buildMetricCard('General Feelings', 'Good', Icons.mood),
+                  ],
                 ),
               ),
             ],
-          );
-        },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMetricCard(String title, String value, IconData icon) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(20),
+        leading: Icon(
+          icon,
+          size: 40,
+          color: Colors.teal,
+        ),
+        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(value, style: TextStyle(fontSize: 18)),
+        trailing: Icon(
+          Icons.edit,
+          color: Colors.blueGrey,
+        ),
       ),
     );
   }
 }
+
