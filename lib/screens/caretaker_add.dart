015e858df5585804317a812bbe05b89ab87a0af5
Gregg Moscosa <gregitis@outlook.com>
2023-11-20 06:30:50 -0600

    Added List of Caretaker for user view

--- lib/screens/caretaker_add.dart
+++ lib/screens/caretaker_add.dart
@@ -42,57 +42,73 @@ class _CaretakerAddScreenState extends State<CaretakerAddScreen> {
   }
 
   Future<void> _addClient() async {
-  if (_clients.isNotEmpty) {
-    final currentUser = _auth.currentUser;
-    if (currentUser == null) {
-      // Handle the case where the user is not signed in
-      return;
-    }
+    if (_clients.isNotEmpty) {
+      final currentUser = _auth.currentUser;
+      if (currentUser == null) {
+        // Handle the case where the user is not signed in
+        return;
+      }
 
-    final caretakerUid = _clients[0]['uid'];
+      final caretakerUid = _clients[0]['uid'];
 
-    // Check if the user is already in the caretaker's clientList
-    final clientListSnapshot = await _firestore
-        .collection('Clients')
-        .doc(caretakerUid)
-        .get();
+      // Check if the user is already in the caretaker's clientList
+      final clientListSnapshot = await _firestore
+          .collection('Clients')
+          .doc(caretakerUid)
+          .get();
 
-    final clientList = (clientListSnapshot.data() as Map<String, dynamic>)['clientList'] ?? [];
-    final isAlreadyAdded = clientList.any((client) => client['uid'] == currentUser.uid);
+      final clientList =
+          (clientListSnapshot.data() as Map<String, dynamic>)['clientList'] ?? [];
+      final isAlreadyAdded = clientList
+          .any((client) => client['uid'] == currentUser.uid);
 
-    if (isAlreadyAdded) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('You are already added to this caretaker\'s client list.'),backgroundColor: Colors.red),
-      );
-    } else {
-      // Add the currently signed-in user to the caretaker's clientList
-      await _firestore.collection('Clients').doc(caretakerUid).set({
-        'clientList': FieldValue.arrayUnion([
+      if (isAlreadyAdded) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'You are already added to this caretaker\'s client list.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      } else {
+        // Add the currently signed-in user to the caretaker's clientList
+        await _firestore.collection('Clients').doc(caretakerUid).set(
           {
-            'uid': currentUser.uid,
-            'displayName': currentUser.displayName,
-            'email': currentUser.email,
-          }
-        ])
-      }, SetOptions(merge: true));
+            'clientList': FieldValue.arrayUnion([
+              {
+                'uid': currentUser.uid,
+                'displayName': currentUser.displayName,
+                'email': currentUser.email,
+              }
+            ])
+          },
+          SetOptions(merge: true),
+        );
 
-      // Add the caretaker to the user's CaretakerList
-      await _firestore.collection('CaretakerList').doc(currentUser.uid).set({
-        'caretakers': FieldValue.arrayUnion([
+        // Add the caretaker to the user's CaretakerList
+        await _firestore.collection('CaretakerList').doc(currentUser.uid).set(
           {
-            'uid': caretakerUid,
-            'displayName': _clients[0]['displayName'],
-            'email': _clients[0]['email'],
-          }
-        ])
-      }, SetOptions(merge: true));
+            'caretakers': FieldValue.arrayUnion([
+              {
+                'uid': caretakerUid,
+                'displayName': _clients[0]['displayName'],
+                'email': _clients[0]['email'],
+              }
+            ])
+          },
+          SetOptions(merge: true),
+        );
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('User added successfully to caretaker\'s client list.'),backgroundColor: Colors.green),
-      );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'User added successfully to caretaker\'s client list.'),
+            backgroundColor: Colors.green,
+          ),
+        );
+      }
     }
   }
-}
 
   Future<void> _fetchContacts() async {
     DocumentSnapshot snapshot = await _firestore
@@ -108,16 +124,37 @@ class _CaretakerAddScreenState extends State<CaretakerAddScreen> {
     }
   }
 
+  Future<void> _fetchCaretakers() async {
+    final currentUser = _auth.currentUser;
+    if (currentUser == null) {
+      // Handle the case where the user is not signed in
+      return;
+    }
+
+    final caretakerListSnapshot =
+        await _firestore.collection('CaretakerList').doc(currentUser.uid).get();
+
+    if (caretakerListSnapshot.exists) {
+      final caretakersFromDb =
+          (caretakerListSnapshot.data() as Map<String, dynamic>)['caretakers'] ??
+              [];
+      setState(() {
+        _caretakersList = List<Map<String, dynamic>>.from(caretakersFromDb);
+      });
+    }
+  }
+
   void _signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => LandingScreen()));
   }
-  
+
   @override
   void initState() {
     super.initState();
     _fetchContacts();
+    _fetchCaretakers(); // Call the method to load caretakers
   }
 
   @override
@@ -139,50 +176,76 @@ class _CaretakerAddScreenState extends State<CaretakerAddScreen> {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: _isSearchBarVisible
-                  ? Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: _emailController,
-                            decoration:
-                                InputDecoration(hintText: "Search by email..."),
+              child: Column(
+                children: [
+                  _isSearchBarVisible
+                      ? Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  hintText: "Search by email...",
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.search),
+                              onPressed: () {
+                                _searchByEmail();
+                              },
+                            ),
+                          ],
+                        )
+                      : ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _isSearchBarVisible = true;
+                            });
+                          },
+                          child: Text('Search'),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              const Color.fromARGB(255, 30, 71, 104),
+                            ),
                           ),
                         ),
-                        IconButton(
-                          icon: Icon(Icons.search),
-                          onPressed: () {
-                            // Trigger the search operation when clicking the icon
-                            _searchByEmail();
-                          },
+                  if (_caretakersList.isNotEmpty)
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal: 16.0,
+                          ),
+                          child: Text(
+                            'Current Caretakers',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        ..._caretakersList.map(
+                          (caretaker) => ListTile(
+                            title: Text(caretaker['displayName'] ?? ''),
+                            subtitle: Text(caretaker['email'] ?? ''),
+                          ),
                         ),
                       ],
-                    )
-                  : Align(
-                      alignment: Alignment.center,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _isSearchBarVisible = true;
-                          });
-                        },
-                        child: Text('Search'),
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color.fromARGB(255, 30, 71, 104)),
-                        ),
+                    ),
+                  if (_clients.isNotEmpty)
+                    ListTile(
+                      title: Text(_clients[0]['displayName'] ?? ''),
+                      subtitle: Text(_clients[0]['email'] ?? ''),
+                      trailing: ElevatedButton(
+                        child: Text('Add'),
+                        onPressed: _addClient,
                       ),
                     ),
-            ),
-            if (_clients.isNotEmpty)
-              ListTile(
-                title: Text(_clients[0]['displayName'] ?? ''),
-                subtitle: Text(_clients[0]['email'] ?? ''),
-                trailing: ElevatedButton(
-                  child: Text('Add'),
-                  onPressed: _addClient,
-                ),
+                ],
               ),
+            ),
           ],
         ),
       ),
