ac024b3dcdbaea2d9481bbd73a44fd263b5ec2a2
Gregg Moscosa <gregitis@outlook.com>
2023-11-20 04:36:23 -0600

    Caretaker User able to add appts/meds for Client users

--- lib/screens/manage_client.dart
+++ lib/screens/manage_client.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'super_medication_form.dart';
 
 class ManageClientScreen extends StatefulWidget {
   final String clientUid;
@@ -12,27 +14,35 @@ class ManageClientScreen extends StatefulWidget {
 }
 
 class _ManageClientScreenState extends State<ManageClientScreen> {
-  final _firestore = FirebaseFirestore.instance;
-  List<Map<String, dynamic>> _clientAppointments = [];
+  final _medsCollection = FirebaseFirestore.instance.collection('medications');
+  final _auth = FirebaseAuth.instance;
 
+  Stream<List<Map<String, dynamic>>?> medicationsStream = Stream.value([]);
   bool _showAppointments = true; // To toggle between Appointments and Medications
 
   @override
   void initState() {
     super.initState();
-    _loadClientAppointments();
+    medicationsStream = listenToMedications();
   }
 
-  void _loadClientAppointments() async {
-    final clientAppointmentsRef =
-        _firestore.collection('Appointments').doc(widget.clientUid);
+  Stream<List<Map<String, dynamic>>> listenToMedications() {
+    final user = _auth.currentUser;
+    final uId = user?.uid;
 
-    final clientAppointmentsSnapshot = await clientAppointmentsRef.get();
-    if (clientAppointmentsSnapshot.exists) {
-      setState(() {
-        _clientAppointments = List<Map<String, dynamic>>.from(
-            clientAppointmentsSnapshot.data()!['events']);
+    if (uId != null) {
+      return _medsCollection.doc(uId).snapshots().map((doc) {
+        if (doc.exists) {
+          final List medsFromDB =
+              (doc.data() as Map<String, dynamic>)['medicationsList'] ?? [];
+
+          return List<Map<String, dynamic>>.from(medsFromDB);
+        } else {
+          return [];
+        }
       });
+    } else {
+      return Stream.value([]);
     }
   }
 
@@ -40,7 +50,8 @@ class _ManageClientScreenState extends State<ManageClientScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Manage Client'), backgroundColor: const Color.fromARGB(255, 30, 71, 104),
+        title: Text('Manage Client'),
+        backgroundColor: const Color.fromARGB(255, 30, 71, 104),
         actions: [
           IconButton(
             icon: Icon(Icons.calendar_today),
@@ -56,43 +67,13 @@ class _ManageClientScreenState extends State<ManageClientScreen> {
               setState(() {
                 _showAppointments = false;
               });
-              // Navigate to MedicationsScreen or show medications content
-              // Replace the next line with your actual navigation or content
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => MedicationsScreen()),
-              // );
             },
           ),
         ],
       ),
       body: _showAppointments
-          ? (_clientAppointments.isEmpty
-              ? Center(
-                  child: Text(
-                    'No appointments found for this client.',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                )
-              : ListView.builder(
-                  itemCount: _clientAppointments.length,
-                  itemBuilder: (context, index) {
-                    final appointment = _clientAppointments[index];
-                    final title = appointment['title'] ?? '';
-                    final date = appointment['date'].toDate();
-
-                    return Card(
-                      child: ListTile(
-                        title: Text(title),
-                        subtitle: Text(
-                            'Date: ${DateFormat('MM-dd-yyyy').format(date)}'),
-                      ),
-                    );
-                  },
-                ))
-          : Container(
-              // This is where your Medications content will be displayed
-            ),
+          ? AppointmentsContent(clientUid: widget.clientUid)
+          : MedicationsContent(clientUid: widget.clientUid),
       floatingActionButton: _showAppointments
           ? FloatingActionButton(
               onPressed: () {
@@ -101,7 +82,26 @@ class _ManageClientScreenState extends State<ManageClientScreen> {
               child: Icon(Icons.add),
               backgroundColor: const Color.fromARGB(255, 30, 71, 104),
             )
-          : null, // Hide FloatingActionButton when showing Medications
+          : ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SuperMedicationFormScreen(selectedUserId: widget.clientUid),
+                  ),
+                ).then((value) {
+                  if (value == true) {
+                    setState(() {});
+                  }
+                });
+              },
+              child: Text("Add Medication"),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(16),
+                textStyle: const TextStyle(fontSize: 18),
+                primary: const Color.fromARGB(255, 30, 71, 104),
+              ),
+            ),
     );
   }
 
@@ -174,7 +174,7 @@ class _ManageClientScreenState extends State<ManageClientScreen> {
   Future<void> _saveClientAppointmentToFirestore(
       {required String title, required DateTime date}) async {
     final clientAppointmentsRef =
-        _firestore.collection('Appointments').doc(widget.clientUid);
+        FirebaseFirestore.instance.collection('Appointments').doc(widget.clientUid);
     List<Map<String, dynamic>> clientAppointments = [];
     final clientAppointmentsSnapshot = await clientAppointmentsRef.get();
     if (clientAppointmentsSnapshot.exists) {
@@ -183,6 +183,157 @@ class _ManageClientScreenState extends State<ManageClientScreen> {
     }
     clientAppointments.add({'title': title, 'date': date.toUtc()});
     await clientAppointmentsRef.set({'events': clientAppointments});
-    _loadClientAppointments();
   }
 }
+
+class MedicationsContent extends StatefulWidget {
+  final String clientUid;
+
+  MedicationsContent({required this.clientUid});
+
+  @override
+  _MedicationsContentState createState() => _MedicationsContentState();
+}
+
+class _MedicationsContentState extends State<MedicationsContent> {
+  final _medsCollection = FirebaseFirestore.instance.collection('medications');
+  final _auth = FirebaseAuth.instance;
+
+  Stream<List<Map<String, dynamic>>> medicationsStream =
+      Stream.value([]); // Initialize with an empty stream
+
+  @override
+  void initState() {
+    super.initState();
+    medicationsStream = listenToMedications(widget.clientUid);
+  }
+
+  Stream<List<Map<String, dynamic>>> listenToMedications(String clientUid) {
+    // Use the provided clientUid instead of the logged-in user's uid
+    return _medsCollection.doc(clientUid).snapshots().map((doc) {
+      if (doc.exists) {
+        final List medsFromDB =
+            (doc.data() as Map<String, dynamic>)['medicationsList'] ?? [];
+
+        return List<Map<String, dynamic>>.from(medsFromDB);
+      } else {
+        return [];
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<Map<String, dynamic>>>(
+      stream: medicationsStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: Text('Loading Medications...'));
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          final medications = snapshot.data ?? [];
+
+          return medications.isEmpty
+              ? Center(
+                  child: Text(
+                    'No medications found for this client.',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: medications.length,
+                  itemBuilder: (context, index) {
+                    final medication = medications[index];
+                    final name = medication['name'] ?? '';
+                    final quantity = medication['quantity'] ?? '';
+                    final frequency = medication['frequency'] ?? '';
+                    final intakeInstructions =
+                        medication['intakeInstructions']?.join(", ") ?? '';
+
+                    return Card(
+                      child: ListTile(
+                        title: Text(name),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Quantity: $quantity'),
+                            Text('Frequency: $frequency'),
+                            Text('Instructions: $intakeInstructions'),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+        }
+      },
+    );
+  }
+}
+
+class AppointmentsContent extends StatefulWidget {
+  final String clientUid;
+
+  AppointmentsContent({required this.clientUid});
+
+  @override
+  _AppointmentsContentState createState() => _AppointmentsContentState();
+}
+
+class _AppointmentsContentState extends State<AppointmentsContent> {
+  final _firestore = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> _clientAppointments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadClientAppointments();
+  }
+
+  void _loadClientAppointments() {
+    final clientAppointmentsRef =
+        _firestore.collection('Appointments').doc(widget.clientUid);
+
+    clientAppointmentsRef.snapshots().listen((clientAppointmentsSnapshot) {
+      if (clientAppointmentsSnapshot.exists) {
+        setState(() {
+          _clientAppointments = List<Map<String, dynamic>>.from(
+              clientAppointmentsSnapshot.data()!['events']);
+        });
+      } else {
+        setState(() {
+          _clientAppointments = [];
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _clientAppointments.isEmpty
+        ? Center(
+            child: Text(
+              'No appointments found for this client.',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        : ListView.builder(
+            itemCount: _clientAppointments.length,
+            itemBuilder: (context, index) {
+              final appointment = _clientAppointments[index];
+              final title = appointment['title'] ?? '';
+              final date = appointment['date'].toDate();
+
+              return Card(
+                child: ListTile(
+                  title: Text(title),
+                  subtitle:
+                      Text('Date: ${DateFormat('MM-dd-yyyy').format(date)}'),
+                ),
+              );
+            },
+          );
+  }
+}
+
